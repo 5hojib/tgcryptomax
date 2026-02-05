2140c2ddc44f72a47c7027811d61a42dc36e5482
5hojib <yesiamshojib@gmail.com>
2026-02-05 22:33:54 +0600

    update

--- tgcryptomax/tgcryptomax.c
+++ tgcryptomax/tgcryptomax.c
@@ -239,10 +239,5 @@ static struct PyModuleDef module = {
 };
 
 PyMODINIT_FUNC PyInit_tgcryptomax(void) {
-    PyObject *m = PyModule_Create(&module);
-    if (m == NULL) return NULL;
-#ifdef Py_GIL_DISABLED
-    PyUnstable_Module_SetGIL(m, Py_MOD_GIL_NOT_USED);
-#endif
-    return m;
+    return PyModule_Create(&module);
 }
